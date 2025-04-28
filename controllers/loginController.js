d674a663e7e98dfae235fec573c94d5db2552395
Sheikh-Redoan <sheikhredoan99@gmail.com>
2025-04-29 00:01:51 +0600

    Login authentication done

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,8 +1,45 @@
-function loginController(req,res) {
-    const {email, password} = req.body
+const emailValidation = require("../helpers/emailvalidation");
+const userSchema = require("../models/userSchema");
+const bcrypt = require('bcrypt');
 
-
-
-
+async function loginController(req, res) {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.json({
+      error: "Please give an email",
+    });
+  }
+  if (!emailValidation(email)) {
+    res.json({
+      error: "email is not vaild",
+    });
+  }
+  if (!password) {
+    return res.json({
+      error: "Please give a password",
+    });
+  }
+  const existingUser = await userSchema.find({email})
+  if(existingUser.length > 0){
+    if (existingUser[0].isVerified == false) {
+        return res.json({error: "email is not verified"})
+    }else{
+        bcrypt.compare(password, existingUser[0].password, function(err, result) {
+            if(result){
+                res.status(200).json({
+                    message: "Login Successfully done"
+                })
+            }else{
+                res.json({
+                    error: "Invalid Password"
+                })
+            }
+        });
+    }
+  }else{
+    res.json({
+        message: "Email is not Valid"
+    })
+  }
 }
-module.exports = loginController
+module.exports = loginController;
--- router/api/authentication.js
+++ router/api/authentication.js
@@ -2,10 +2,12 @@ const express = require("express");
 const registrationController = require("../../controllers/registrationController");
 const otpController = require("../../controllers/otpController");
 const resendOtpController = require("../../controllers/resendOtpController");
+const loginController = require("../../controllers/loginController");
 const route = express.Router();
 
 route.post("/registration", registrationController);
 route.post("/otpverify", otpController);
 route.post("/resendotp", resendOtpController);
+route.post("/login", loginController);
 
 module.exports = route;
